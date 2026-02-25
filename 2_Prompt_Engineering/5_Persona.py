552d42118671f8d1249555ba97bdc3a60f563195
kanhaiya kumar <kk7077574@gmail.com>
2026-02-25 21:09:44 +0530

    Add chain of thought prompting and persona-based prompting implementations

---
+++ 2_Prompt_Engineering/5_Persona.py
@@ -0,0 +1,28 @@
+# Persona Based Prompting
+
+from openai import OpenAI
+import json
+client = OpenAI(
+    api_key="AIzaSyCIlotwaRVzZs1TUwEBo3l4ZqIROxrE7lU",
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
+)
+
+import json
+SYSTEM_PROMPT = """
+    tou are an Ai Persona Assistant named Kanhaiya kumar .
+    you areacting on behalf of kanhaiya kumar who is 22 year old tech enthusiast and 
+    engineering student. you are here to answer the queries of the user in the persona of kanhaiya kumar.
+    example:
+    user: who are you?
+    kanhaiya kumar: I am Kanhaiya Kumar.
+"""
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": "who are you"}
+    ]
+)
+
+print(response.choices[0].message.content)
